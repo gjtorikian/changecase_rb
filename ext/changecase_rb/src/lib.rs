63cf8cd63b642d1557454319cf19dbab778b65c7
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2023-11-04 15:11:41 -0400

    Add Rust/Magnus glue

--- ext/changecase_rb/src/lib.rs
+++ ext/changecase_rb/src/lib.rs
@@ -1,12 +1,27 @@
-use magnus::{function, prelude::*, Error, Ruby};
+use changecase::Case;
+use changecase::ChangeCase;
 
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {subject}!")
+use magnus::{class, define_class, function, method, prelude::*, Error};
+
+#[magnus::wrap(class = "ChangecaseRb")]
+struct Changecase {
+    str: String,
+}
+
+impl Changecase {
+    fn new(str: String) -> Self {
+        Self { str }
+    }
+
+    fn to_alt_case(&self) -> String {
+        self.str.to_altcase(Case::Upper)
+    }
 }
 
 #[magnus::init]
-fn init(ruby: &Ruby) -> Result<(), Error> {
-    let module = ruby.define_module("ChangecaseRb")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
+fn init() -> Result<(), Error> {
+    let class = define_class("ChangecaseRb", class::object())?;
+    class.define_singleton_method("new", function!(Changecase::new, 1))?;
+    class.define_method("to_alt_case", method!(Changecase::to_alt_case, 0))?;
     Ok(())
 }
